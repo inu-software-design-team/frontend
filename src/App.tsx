bff0a058e391a05abc0d73c383ef6e8018f5d3fc
hyewon <helena030828@naver.com>
2025-04-02 19:41:15 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="w-full h-full flex flex-col bg-white">
+    <Router>
+        <Routes>
+            <Route />
+        </Routes>
+    </Router>
+    </div>
+  )
+}
+
+export default App
