8c3dbf4102f84045f85ce28c0af45242d4a3e049
herokwon <contact@herokwon.dev>
2025-04-16 20:29:52 +0900

    chore: App Router 기반 페이지 및 레이아웃 생성

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: {
+  children: Readonly<React.ReactNode>;
+}) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
