fc4a48202f4ba34261c7e78899ecf82247fb1e26
herokwon <contact@herokwon.dev>
2025-04-16 21:26:42 +0900

    chore: Pretendard 글꼴 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
+import LocalFont from 'next/font/local';
+
 import '../globals.css';
+
+const pretendard = LocalFont({
+  src: '../../node_modules/pretendard/dist/web/variable/woff2/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +14,7 @@ export default function RootLayout({
   children: Readonly<React.ReactNode>;
 }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.className}>
       <body>{children}</body>
     </html>
   );
