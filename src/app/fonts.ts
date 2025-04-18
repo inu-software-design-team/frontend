37111b52e53c38a699a8a0416ebe90710c165d4b
herokwon <contact@herokwon.dev>
2025-04-18 14:19:58 +0900

    move: `Pretendard` 글꼴 참조 위치 변경 (`src/app/layout.tsx` → `src/app/fonts`)

---
+++ src/app/fonts.ts
@@ -0,0 +1,8 @@
+import LocalFont from 'next/font/local';
+
+export const Pretendard = LocalFont({
+  src: '../../node_modules/pretendard/dist/web/variable/woff2/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
-import LocalFont from 'next/font/local';
+import { Pretendard } from './fonts';
 
 import './globals.css';
-
-const pretendard = LocalFont({
-  src: '../../node_modules/pretendard/dist/web/variable/woff2/PretendardVariable.woff2',
-  display: 'swap',
-  weight: '45 920',
-});
 
 export default function RootLayout({
   children,
@@ -14,7 +8,7 @@ export default function RootLayout({
   children: Readonly<React.ReactNode>;
 }) {
   return (
-    <html lang="ko" className={pretendard.className}>
+    <html lang="ko" className={Pretendard.className}>
       <body className="flex h-screen w-screen flex-col bg-[#F1F5F9]">
         {children}
       </body>
