a13b67aed2c2e23f29f8332ab618c5d8c7181148
herokwon <contact@herokwon.dev>
2025-04-18 14:21:31 +0900

    style: 글꼴 스타일 적용 방식 변경 (`class` → `variable`)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ export default function RootLayout({
   children: Readonly<React.ReactNode>;
 }) {
   return (
-    <html lang="ko" className={Pretendard.className}>
-      <body className="flex h-screen w-screen flex-col bg-[#F1F5F9]">
+    <html lang="ko" className={Pretendard.variable}>
+      <body className="font-pretendard flex h-screen w-screen flex-col bg-[#F1F5F9]">
         {children}
       </body>
     </html>
