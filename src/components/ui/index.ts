35cad5ca20b04cdb011e5b6fa19cf410fc510af8
herokwon <contact@herokwon.dev>
2025-04-29 22:33:32 +0900

    feat(ui): `Table` 컴포넌트 구현

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,5 @@
 export { default as Icon } from './Icon';
 export { default as IconButton } from './IconButton';
 export { default as Spinner } from './Spinner';
+export { default as Table } from './Table';
 export { default as TextButton } from './TextButton';
