0a4f794a13096f74ae67c2996c98c76992285843
herokwon <contact@herokwon.dev>
2025-05-01 15:27:15 +0900

    test(layout): `SideNav` 컴포넌트 `usePathname` 모킹 추가

--- src/layouts/SideNav.test.tsx
+++ src/layouts/SideNav.test.tsx
@@ -1,4 +1,8 @@
+import { vi } from 'vitest';
+
 import { render, screen } from '@testing-library/react';
+
+import { usePathname } from 'next/navigation';
 
 import { NAV_ITEMS } from 'data';
 
@@ -6,12 +10,14 @@ import SideNav from './SideNav';
 
 describe('SideNav 레이아웃 컴포넌트 테스트', () => {
   it('컴포넌트를 정상적으로 렌더링해야 합니다.', () => {
+    vi.mocked(usePathname).mockReturnValue('/dashboard');
     const { container } = render(<SideNav />);
 
     expect(container).toBeInTheDocument();
   });
 
   it('네비게이션 요소들을 정상적으로 렌더링해야 합니다.', () => {
+    vi.mocked(usePathname).mockReturnValue('/dashboard');
     render(<SideNav />);
     const links = screen.getAllByRole('link');
 
