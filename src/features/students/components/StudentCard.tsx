7758ab4fdc78acb2a8a86efb87d4d7aedc090504
herokwon <contact@herokwon.dev>
2025-05-01 15:32:45 +0900

    fix: `StudentCard` 컴포넌트 링크 경로 오류 수정

--- src/features/students/components/StudentCard.tsx
+++ src/features/students/components/StudentCard.tsx
@@ -21,11 +21,12 @@ const StudentCard = ({
   number,
   name,
 }: StudentCardProps) => {
+  const basePathname = pathname.split('/').slice(0, 3).join('/');
   const studentInfo = `${grade}학년 ${classNumber}반 ${number}번`;
 
   return (
     <Link
-      href={`${pathname}/${id}`}
+      href={`${basePathname}/${id}`}
       className={`flex w-full items-center gap-4 gap-x-4 rounded-md px-4 py-3 transition-colors ${pathname.includes(id) ? 'bg-primary-light-hover text-primary-hover stroke-primary-hover' : 'hover:not-[data-status="disabled"]:bg-secondary stroke-current'}`}
     >
       <Icon src={CircleUser} size={48} />
