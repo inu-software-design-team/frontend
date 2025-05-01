c25f8aca1962eddbca19827f1755d3275be168fb
herokwon <contact@herokwon.dev>
2025-05-01 22:01:38 +0900

    refactor(layout): `PageHeader` 컴포넌트 경로 탐색 로직 최적화

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,30 +1,33 @@
 'use client';
 
-import { usePathname, useRouter } from 'next/navigation';
+import { useMemo } from 'react';
+
+import { usePathname } from 'next/navigation';
 
 import { NAV_ITEMS } from 'data';
+
+import type { ElementType } from 'types';
 
 import { Icon } from 'components/ui';
 
 const PageHeader = () => {
-  const { replace } = useRouter();
   const pathname = usePathname();
-  const pageInfo = NAV_ITEMS.find(({ path }) => {
-    const secondSlashIndex = pathname.indexOf('/', 1);
-    if (secondSlashIndex === -1) return path === pathname;
+  const pageInfo = useMemo(
+    () =>
+      NAV_ITEMS.reduce<ElementType<typeof NAV_ITEMS> | null>(
+        (bestMatch, item) => {
+          if (pathname === item.path || pathname.startsWith(item.path)) {
+            if (!bestMatch || item.path.length > bestMatch.path.length)
+              return item;
+          }
+          return bestMatch;
+        },
+        null,
+      ),
+    [pathname],
+  );
 
-    const thirdSlashIndex = pathname.indexOf('/', pathname.indexOf('/', 1) + 1);
-    return thirdSlashIndex === -1
-      ? path === pathname
-      : path === pathname.substring(0, thirdSlashIndex);
-  });
-
-  if (!pageInfo) {
-    replace('/404', { scroll: false });
-    return null;
-  }
-
-  return (
+  return !pageInfo ? null : (
     <div className="bg-primary h-60 w-full p-8">
       <div className="flex items-center gap-x-4 stroke-white text-white">
         <Icon src={pageInfo.icon} size={32} />
