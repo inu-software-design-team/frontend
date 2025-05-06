31227b3b4a454f2a1f6ddaee46b7313c13d96f75
herokwon <contact@herokwon.dev>
2025-05-07 06:37:29 +0900

    fix(middleware): 세션 체크 및 CSRF 토큰 요청 로직 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+import { cookies } from 'next/headers';
 import { type NextRequest, NextResponse } from 'next/server';
 
 export const middleware = async (request: NextRequest) => {
@@ -5,6 +6,47 @@ export const middleware = async (request: NextRequest) => {
   const searchParams = request.nextUrl.searchParams;
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-pathname', pathname);
+
+  const cookieStore = await cookies();
+  const sessionId = cookieStore.get('connect.sid')?.value ?? '';
+
+  if (sessionId.length > 0) {
+    try {
+      const response = await fetch(
+        'http://localhost:4000/api/v1/users/session-check',
+        {
+          headers: {
+            Cookie: cookieStore.toString(),
+          },
+        },
+      );
+
+      const res = await response.json();
+      console.log(res);
+    } catch (error) {
+      console.error(error instanceof Error ? error.message : String(error));
+    }
+  }
+
+  if (pathname === '/dashboard' && sessionId.length === 0) {
+    const cookieStore = await cookies();
+
+    try {
+      const response = await fetch(
+        'http://localhost:4000/api/v1/users/csrf-token',
+        {
+          headers: {
+            Cookie: cookieStore.toString(),
+          },
+        },
+      );
+
+      const { csrfToken } = await response.json();
+      console.log(csrfToken);
+    } catch (error) {
+      console.error(error instanceof Error ? error.message : String(error));
+    }
+  }
 
   if (pathname === '/dashboard' && searchParams.size > 0) {
     return NextResponse.redirect(pathname);
