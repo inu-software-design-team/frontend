d7a5dd20a98e0ac3f686310829abb8b08fc4232a
herokwon <contact@herokwon.dev>
2025-05-06 20:45:15 +0900

    refactor(layout): `DashboardContentBox` 컴포넌트 렌더링 위치 변경

--- src/app/dashboard/grade/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardContentBox } from 'layouts';
-
-export default function GradeLayout({
-  children,
-}: {
-  children: Readonly<React.ReactNode>;
-}) {
-  return <DashboardContentBox>{children}</DashboardContentBox>;
-}
