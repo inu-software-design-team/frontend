b6aecb2d37438e1da88c52d08204c3fe4b4d77a8
herokwon <contact@herokwon.dev>
2025-05-18 12:58:49 +0900

    feat(types): 유틸리티/파라미터 관련 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,9 @@
 export * from './student';
 
+export type StrictOmit<T, K extends keyof T> = {
+  [P in keyof T]: P extends K ? never : T[P];
+};
+export type StrictExtract<T, K extends keyof T> = K;
 export type ArrayElementType<T> = T extends (infer U)[] ? U : T;
 
 export const ELEMENT_STATUS = [
@@ -29,6 +33,10 @@ export type IdParams = {
   id: string;
 };
 export type AsyncIdParams = Promise<IdParams>;
+export type SearchParams = {
+  [key: string]: string | string[] | undefined;
+};
+export type AsyncSearchParams = Promise<SearchParams>;
 
 export type WithLinkNavigation = {
   href?: {
