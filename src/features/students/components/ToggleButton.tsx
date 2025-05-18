1b1299777edf992a2e2b24e897753aff8a68ab09
herokwon <contact@herokwon.dev>
2025-05-18 12:57:29 +0900

    design (students): `ToggleButton` 컴포넌트 표시 조건 추가

--- src/features/students/components/ToggleButton.tsx
+++ src/features/students/components/ToggleButton.tsx
@@ -1,20 +1,27 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
+
 import { IconButton } from 'components/ui';
 
 const ToggleButton = () => {
+  const pathname = usePathname();
+  const shouldShowToggleButton = pathname !== '/dashboard';
+
   return (
-    <IconButton
-      icon="search"
-      color="default"
-      spacing="compact"
-      className="fixed top-16 right-0 z-30 my-4 rounded-r-none xl:hidden"
-      onClick={e => {
-        e.currentTarget.parentElement
-          ?.querySelector('#student-list')
-          ?.classList.add('show');
-      }}
-    />
+    shouldShowToggleButton && (
+      <IconButton
+        icon="search"
+        color="default"
+        spacing="compact"
+        className="fixed top-16 right-0 z-30 my-4 rounded-r-none xl:hidden"
+        onClick={e => {
+          e.currentTarget.parentElement
+            ?.querySelector('#student-list')
+            ?.classList.add('show');
+        }}
+      />
+    )
   );
 };
 
