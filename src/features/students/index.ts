9f8275c463f230844fbccee79c906ad40a0922e8
herokwon <contact@herokwon.dev>
2025-05-21 14:07:34 +0900

    feat(students): `StudentProfile` 컴포넌트 구현

--- src/features/students/index.ts
+++ src/features/students/index.ts
@@ -1,4 +1,5 @@
 export * from './components/actions';
 
 export { default as StudentList } from './components/StudentList';
+export { default as StudentProfile } from './components/StudentProfile';
 export { default as ToggleButton } from './components/ToggleButton';
