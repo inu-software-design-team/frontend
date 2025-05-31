3294413d307b987e210f90240eb99d64ba53c66c
herokwon <contact@herokwon.dev>
2025-05-31 22:46:54 +0900

    refactor(layout): 메인 페이지 콘텐츠 영역 렌더링 조건 제거

--- src/layouts/DashboardContentBox.tsx
+++ src/layouts/DashboardContentBox.tsx
@@ -1,22 +1,18 @@
 'use client';
 
-import { usePathname, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 
 import { Loader } from 'components';
 
 const DashboardContentBox = ({ children }: React.PropsWithChildren) => {
-  const pathname = usePathname();
   const searchParams = useSearchParams();
-  const shouldShowContentBox = pathname !== '/dashboard';
   const isLoading = searchParams.get('status') === 'loading';
 
   return (
-    shouldShowContentBox && (
-      <div className="shadow-drop bg-default sticky top-16 left-0 flex h-[calc(100vh-4rem-8rem)] w-full max-w-7xl flex-col gap-12 rounded-md p-8 max-sm:rounded-b-none">
-        {children}
-        <Loader isLoading={isLoading} />
-      </div>
-    )
+    <div className="shadow-drop bg-default sticky top-16 left-0 flex h-[calc(100vh-4rem-8rem)] w-full flex-col gap-12 rounded-md p-8 max-sm:rounded-b-none">
+      {children}
+      <Loader isLoading={isLoading} />
+    </div>
   );
 };
 
