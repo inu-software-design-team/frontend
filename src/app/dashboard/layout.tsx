00810b96a641eaf9913479072cb5fa5d9d6f8e75
herokwon <contact@herokwon.dev>
2025-05-31 17:57:34 +0900

    feat(layout): `SideNav` 컴포넌트 역할 기반 접근 제한 설정 추가

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,6 +8,7 @@ import {
   SideNav,
 } from 'layouts';
 
+import { getUserInfo } from 'features/auth';
 import {
   getStudentList,
   getYearListForStudent,
@@ -20,24 +21,29 @@ export default async function DashboardLayout({
 }: {
   children: Readonly<React.ReactNode>;
 }) {
-  const years = await getYearListForStudent();
-  const students: StudentInfo[] =
+  const { role } = await getUserInfo();
+
+  const years = role === 'teacher' ? await getYearListForStudent() : [];
+  const students = (
     years.length === 0
       ? []
       : await getStudentList({
           year: years[0].year,
-        });
+        })
+  ) satisfies StudentInfo[];
 
   return (
     <>
       <Header />
       <div className="grid w-full grid-cols-[minmax(0,_max-content)_1fr] max-xl:grid-cols-1">
-        <SideNav initialNavConfig={await getNavConfig()} />
+        <SideNav role={role} initialNavConfig={await getNavConfig()} />
         <main className="relative w-full">
           <PageHeader />
           <section className="-mt-28 grid w-full grid-cols-1 justify-center gap-x-8 sm:px-4 md:px-8 xl:grid-cols-[minmax(0,_25rem)_minmax(0,_1fr)]">
             <ToggleButton />
-            <StudentList years={years} students={students} />
+            {role === 'teacher' && (
+              <StudentList years={years} students={students} />
+            )}
             <DashboardContentBox>{children}</DashboardContentBox>
           </section>
         </main>
