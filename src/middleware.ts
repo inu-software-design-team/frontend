fb75d164519eb28761a2d3b50cd8587d5c6be9d0
HeroKwon <contact@herokwon.dev>
2025-06-02 14:40:21 +0900

    fix(middleware): 누락된 예외 처리로 인한 오류 수정 (#118)
    * fix: TypeError about `toSorted`
    
    * refactor(students): `StudentProfile` 컴포넌트 비동기 처리 제거
    
    * feat(api): 학생 상담 API 추가 (#106)
    
    * feat(api): 학생 상담 API 추가
    
    * feat: 학생 상담 기능 구현
    
    * feat: 학생 존재 여부 확인 및 처리 기능 추가 (#107)
    
    * feat(students): 학생 존재 여부 확인 및 처리 기능 구현
    
    * feat: 학생 존재 여부 확인 및 처리 로직 적용
    
    * feat: 레이더 차트 기능 추가 (#108)
    
    * chore: `Chart.js` 관련 의존성 추가
    
    * feat(grades): 과목별 레이더 차트 기능 구현
    
    * fix(students): API 경로 대소문자 수정
    
    * fix: 컴포넌트 중복 제거
    
    * fix: prefetch 설정으로 인한 불필요한 네트워크 요청 제거
    
    * fix: 버튼 컴포넌트 중복 제거
    
    * fix(api): 404 오류 처리 추가
    
    * design: `Loader` 컴포넌트 배경색상 스타일 추가
    
    * feat: 역할 기반 접근 제한 설정 추가 (#114)
    
    * feat(layout): `SideNav` 컴포넌트 역할 기반 접근 제한 설정 추가
    
    * feat: 성적 관련 요소 역할 기반 접근 제한 설정 추가
    
    * feat: 상담 관련 요소 역할 기반 접근 제한 설정 추가
    
    * feat: 학생부 관련 요소 역할 기반 접근 제한 설정 추가
    
    * feat: 보고서 관련 요소 역할 기반 접근 제한 설정 추가
    
    * feat: 피드백 관련 요소 역할 기반 접근 제한 설정 추가
    
    * feat(middleware): 역할 기반 페이지 접근 제한 설정 추가
    
    * chore(api): 공통 상수를 이용한 API 경로 통합
    
    * refactor: 역할 기반 접근 제한을 위한 매개변수 재설정
    
    * refactor(layout): 메인 페이지 콘텐츠 영역 렌더링 조건 제거
    
    * design: 메인 페이지 레이아웃 display 스타일 변경 (`grid` → `flex`)
    
    * fix(middleware): 누락된 예외 처리로 인한 오류 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,30 +7,32 @@ export const middleware = async (request: NextRequest) => {
   const searchParams = request.nextUrl.searchParams;
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-pathname', pathname);
+  const cookies = request.cookies;
 
-  const session = await checkSession()
-    .then(res => res)
-    .catch(() => null);
+  const session = cookies.get('connect.sid')?.value ?? '';
   // 세션이 없는 경우
-  if (!session) return NextResponse.redirect(new URL('/', request.url));
+  if (session.length === 0) {
+    if (!(pathname === '/' || pathname.startsWith('/auth')))
+      return NextResponse.redirect(new URL('/', request.url));
+  } else {
+    // 세션이 있는데 인증 페이지로 접근하는 경우
+    if (pathname === '/' || pathname.startsWith('/auth'))
+      return NextResponse.redirect(new URL('/dashboard', request.url));
 
-  // 세션이 있는데 인증 페이지로 접근하는 경우
-  if (pathname === '/' || pathname.startsWith('/auth'))
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+    const { role } = (await checkSession()).user;
+    // 세션이 있고 교사가 아닌 경우
+    if (
+      role !== 'teacher' &&
+      (['student-info', 'report'].some(path =>
+        pathname.startsWith(`/dashboard/${path}`),
+      ) ||
+        pathname.endsWith('manage'))
+    )
+      return NextResponse.redirect(new URL('/dashboard', request.url));
 
-  const { role } = session.user;
-  // 세션이 있고 교사가 아닌 경우
-  if (
-    role !== 'teacher' &&
-    (['student-info', 'report'].some(path =>
-      pathname.startsWith(`/dashboard/${path}`),
-    ) ||
-      pathname.endsWith('manage'))
-  )
-    return NextResponse.redirect(new URL('/dashboard', request.url));
-
-  if (pathname === '/dashboard' && searchParams.size > 0) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+    if (pathname === '/dashboard' && searchParams.size > 0) {
+      return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
   }
 
   return NextResponse.next({
